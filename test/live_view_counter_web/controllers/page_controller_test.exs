22cba2b3720cf9b7da3ffef1f66f5d8ee7c84282
doxxx <doxxx93@gmail.com>
2023-03-18 07:09:08 +0900

    feat: counter.ex and test code

--- test/live_view_counter_web/controllers/page_controller_test.exs
+++ test/live_view_counter_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LiveViewCounterWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "The count is"
   end
 end
